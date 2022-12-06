c174d51f20a3d913837bdda61d5b875b0a24ba85
uytran36 <uytkg@fpt.com.vn>
2022-12-06 17:52:41 +0700

    update admin

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,11 +1,220 @@
+import React, { useEffect, useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  RiseOutlined,
+  PartitionOutlined,
+  CarryOutOutlined,
+  MoneyCollectOutlined,
+} from "@ant-design/icons";
+import {
+  Layout,
+  Menu,
+  theme,
+  Form,
+  Input,
+  Button,
+  Space,
+  Table,
+  Modal,
+  Checkbox,
+} from "antd";
 
+const { Header, Sider, Content } = Layout;
 
 const Admin = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [data, setData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    function requestGetData() {
+      fetch(`${import.meta.env.VITE_API_MOCK}/routes`).then((res) => {
+        res.json().then((res) => {
+          setData(res?.data);
+        });
+      });
+    }
+
+    requestGetData();
+  }, []);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Icon",
+      dataIndex: "icon",
+      key: "icon",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <a>edit</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Layout style={{ height: "100vh" }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <PartitionOutlined />,
+              label: "Routes",
+            },
+            {
+              key: "2",
+              icon: <MoneyCollectOutlined />,
+              label: "Credit Score",
+            },
+            {
+              key: "3",
+              icon: <RiseOutlined />,
+              label: "Coin Rate",
+            },
+            {
+              key: "4",
+              icon: <CarryOutOutlined />,
+              label: "Recent Activities",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Button type="primary" onClick={showModal}>
+            Add route
+          </Button>
+          <Table columns={columns} dataSource={data} />
+          <Modal
+            title="Basic Modal"
+            open={isModalOpen}
+            onOk={handleOk}
+            onCancel={handleCancel}
+          >
+            <Form
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Modal>
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
-
 export default Admin;
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,33 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import "./index.css";
+import { Button, Result } from "antd";
 import Admin from "./Admin";
 
 const router = createBrowserRouter([
   {
     path: "/admin",
     element: <Admin />,
+    // children: [{ path: "/credit-score",  }],
   },
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "*",
+    element: (
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary" onClick={() => (window.location.href = "/")}>
+            Back Home
+          </Button>
+        }
+      />
+    ),
   },
 ]);
 
